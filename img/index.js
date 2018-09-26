dab6508de56dff0c54854d24c012660a8b3fb6e4
Kate Beard <asouthernbellein@gmail.com>
2018-09-26 11:49:18 +0100

    Initial commit

---
+++ img/index.js
@@ -0,0 +1,1 @@
+export jaredleto from "./jaredleto.jpeg";
---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import jaredleto from "./img/jaredleto.jpeg";
+
+const App = () => <div><h1>TAMA<em>GOTH</em>I</h1><img src={jaredleto}/></div>;
+
+render(<App />, document.getElementById('root'));
